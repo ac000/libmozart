f688d16fd49fe63b11c7ed755ee02c204212086a
Andrew Clayton <andrew@digital-domain.net>
2010-01-26 10:58:48 +0000

    Remove unused likely/unlikely defines.

--- libmozart.h
+++ libmozart.h
@@ -9,9 +9,6 @@
 #ifndef _LIBMOZART_H_
 #define _LIBMOZART_H_
 
-#define likely(x)       __builtin_expect (!!(x), 1)
-#define unlikely(x)     __builtin_expect (!!(x), 0)
-
 #include "player-operations.h"
 #include "playlist-operations.h"
 
